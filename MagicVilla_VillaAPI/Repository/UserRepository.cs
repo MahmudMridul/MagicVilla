2413d2404e86cbd620451b0a816f581bef6b74b9
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-08-30 08:53:39 +0600

    Added user repository

---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool IsUniqueUser(string username)
+        {
+            LocalUser user = _db.LocalUsers.FirstOrDefault(user => user.UserName == username);
+            if (user == null) { return true; }
+            return false;
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            LocalUser user = new LocalUser()
+            {
+                Name = registrationRequestDTO.Name,
+                UserName = registrationRequestDTO.UserName,
+                Password = registrationRequestDTO.Password,
+                Role = registrationRequestDTO.Role,
+            };
+            await _db.LocalUsers.AddAsync(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+        }
+    }
+}
