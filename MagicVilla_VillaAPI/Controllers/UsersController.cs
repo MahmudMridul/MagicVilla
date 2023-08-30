75443b87a1204a8dd3929e055f1ce8b5745ecc73
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-08-30 09:51:37 +0600

    Added user controller

---
+++ MagicVilla_VillaAPI/Controllers/UsersController.cs
@@ -0,0 +1,67 @@
+﻿using Azure;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/UsersAuth")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private APIResponse _response;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+            _response = new APIResponse();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<APIResponse>> Login([FromBody] LoginRequestDTO model)
+        {
+            LoginResponseDTO response = await _userRepository.Login(model);
+            if(response.User == null || string.IsNullOrEmpty(response.Token))
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { "Username or password is incorrect" };
+                return _response;
+            }
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.IsSuccess = true;
+            _response.Result = response;
+            return _response;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<APIResponse>> Register([FromBody] RegistrationRequestDTO model)
+        {
+            bool uniqueUser = _userRepository.IsUniqueUser(model.UserName);
+            if(!uniqueUser)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { "Username exists" };
+                return _response;
+            }
+            LocalUser user = await _userRepository.Register(model);
+            if(user == null)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { "Error while regitering" };
+                return _response;
+            }
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.IsSuccess = true;
+            _response.Result = user;
+            return _response;
+        }
+
+    }
+}
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -18,11 +18,10 @@ namespace MagicVilla_VillaAPI
             {
                 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
             });
-
             builder.Services.AddAutoMapper(typeof(MappingConfig));
-
             builder.Services.AddScoped<IVillaRepository, VillaRepository>();
             builder.Services.AddScoped<IVillaNumberRepository, VillaNumberRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
 
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Information()
